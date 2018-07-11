74acbd637ed22ca5891d91cccec25df7c9eae6ff
Friederike Wild <dev@friederikewild.me>
2018-07-11 14:09:53 +0100

    * Define simple glide module

---
+++ app/src/main/java/me/friederikewild/demo/glide/util/SimpleAppGlideModule.kt
@@ -0,0 +1,10 @@
+package me.friederikewild.demo.glide.util
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Default setup for Glide Image Loading Lib
+ */
+@GlideModule
+class SimpleAppGlideModule : AppGlideModule()
