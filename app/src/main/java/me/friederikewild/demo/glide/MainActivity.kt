8f9c1eb9afcefb7a168aa335433f16588b5bdd7d
Friederike Wild <dev@friederikewild.me>
2018-07-11 15:30:19 +0100

    * Add glide loading with rounded corners to a simple layout

--- app/src/main/java/me/friederikewild/demo/glide/MainActivity.kt
+++ app/src/main/java/me/friederikewild/demo/glide/MainActivity.kt
@@ -1,12 +1,28 @@
 package me.friederikewild.demo.glide
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import me.friederikewild.demo.glide.util.GlideApp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // For testing: Url to test profile picture
+        val pictureUrl = "https://upload.wikimedia.org/wikipedia/commons/d/dd/Chuck_Norris_Small.JPG"
+        val radius = resources.getDimensionPixelSize(R.dimen.profile_corner_radius)
+
+        GlideApp.with(this)
+                .load(pictureUrl)
+                .transform(RoundedCorners(radius))
+// .transforms(CenterCrop(), RoundedCorners(radius))
+                .placeholder(R.drawable.placeholder_thumbnail_square_primary)
+                .error(R.drawable.placeholder_thumbnail_square_primary)
+                .transition(DrawableTransitionOptions.withCrossFade()).into(detailProfilePicture)
     }
 }
