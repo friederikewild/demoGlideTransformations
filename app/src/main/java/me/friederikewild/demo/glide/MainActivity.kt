82dda6bc5d88f149d72d40699280da03b49df044
Friederike Wild <dev@friederikewild.me>
2018-07-11 15:46:45 +0100

    * Fix loading of non-square images by adding a CenterCrop transformation

--- app/src/main/java/me/friederikewild/demo/glide/MainActivity.kt
+++ app/src/main/java/me/friederikewild/demo/glide/MainActivity.kt
@@ -2,6 +2,7 @@ package me.friederikewild.demo.glide
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import kotlinx.android.synthetic.main.activity_main.*
@@ -27,8 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         GlideApp.with(this)
                 .load(pictureUrl)
-                .transform(RoundedCorners(radius))
-// .transforms(CenterCrop(), RoundedCorners(radius))
+                .transforms(CenterCrop(), RoundedCorners(radius))
                 .placeholder(R.drawable.placeholder_thumbnail_square_dec_primary)
                 .error(R.drawable.placeholder_thumbnail_square_dec_primary)
                 .transition(DrawableTransitionOptions.withCrossFade()).into(detailProfilePicture)
